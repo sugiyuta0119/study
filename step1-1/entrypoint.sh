c9f5f8b61ed02b91d81eab156c5086f4f2d5042a
sugiyuta0119 <sugiyuta0119@gmail.com>
2025-10-23 15:28:51 +0900

    step1-1 add

---
+++ step1-1/entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+# メインプロセスを管理するためのPID変数
+APP_PID=0
+
+# クリーンアップ処理を定義する関数
+function cleanup {
+  echo "SIGTERMを受信しました。クリーンアップを実行します..."
+  
+  # メインプロセスにシグナルを転送
+  if [ "$APP_PID" -ne 0 ]; then
+    echo "メインプロセスの終了処理に入ります"
+    kill -SIGTERM "$APP_PID"
+    echo "メインプロセスの終了待ちです"
+    while kill -0 "$APP_PID" 2>/dev/null; do
+      sleep 1
+    done
+    echo "メインプロセスが終了しました"
+  fi
+  
+  # その他のクリーンアップ処理（一時ファイルの削除など）
+  echo "クリーンアップが完了しました。"
+  /bin/sync
+  exit 0
+}
+
+# SIGTERMシグナルを捕捉するように設定
+trap cleanup SIGTERM
+
+# --- 起動前処理 ---
+echo "起動前処理を実行..."
+# ここに必要な起動前処理を記述
+# 例: データベースのマイグレーション、設定ファイルの生成
+# migrate_db.sh
+# generate_config.sh
+
+# --- メインプロセスの起動 ---
+echo "メインプロセスを起動します: $@"
+# CMDで指定されたコマンドをバックグラウンドで実行
+"$@" &
+APP_PID=$!
+
+# --- メインプロセスの終了を待機 ---
+# waitコマンドでメインプロセスが終了するまで待機
+while kill -0 "$APP_PID" 2>/dev/null; do
+#  echo "ここループ回ってます"
+  sleep 5
+done
+
+echo "何らかの理由でメインプロセスが終了しました"
+
+# スクリプトのクリーンアップ関数が呼ばれなかった場合に、
+# メインプロセスの終了コードで終了
+exit $?
+
